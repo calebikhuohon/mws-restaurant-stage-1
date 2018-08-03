bd90048f4cfb9c23e4a5e66cbec6a63b81d4e01f
caleb <calebikhuohon@gmail.com>
2018-08-03 21:34:19 +0100

    chore: add API token

--- js/main.js
+++ js/main.js
@@ -77,8 +77,8 @@ initMap = () => {
         zoom: 12,
         scrollWheelZoom: false
       });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={pk.eyJ1IjoiY2FsZWJpa2h1b2hvbiIsImEiOiJjamtlZmVzaW8zenBtM3FtcW9xbm5qcjV3In0.AQLRZAE3loBBmHxLkA1WUw}', {
+    mapboxToken: 'pk.eyJ1IjoiY2FsZWJpa2h1b2hvbiIsImEiOiJjamtlZmVzaW8zenBtM3FtcW9xbm5qcjV3In0.AQLRZAE3loBBmHxLkA1WUw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
