c49822835048fcc2add88a693b8b81aa84e3f1f8
caleb <calebikhuohon@gmail.com>
2018-09-24 11:04:35 +0100

    chore: change data source and port to pull data from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8080 // Change this to your server port
-    return `https://calebikhuohon.github.io/mws-restaurant-stage-1/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    //return `https://calebikhuohon.github.io/mws-restaurant-stage-1/data/restaurants.json`;
+    return fetch('http://localhost:1337/restaurants');
   }
 
   /**
