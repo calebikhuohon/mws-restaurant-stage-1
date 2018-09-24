115a5ce5cc5ec5f4347a3c6eca928708a2eb0fbf
caleb <calebikhuohon@gmail.com>
2018-09-24 15:42:28 +0100

    chore: bug hunting

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -55,7 +55,7 @@ class DBHelper {
 
       } else { // Oops!. Got an error from server.
         // const error = (`Request failed. Returned status of ${xhr.status}`);
-        // callback(error, null);
+         callback(error, null);
 
         console.log('offline. Query will be fetched from idb');
         DBHelper.dbPromise.then(db => {
