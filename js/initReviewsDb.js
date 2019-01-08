07c8b07e6361422d2fd69cda744a905cd416b601
caleb <calebikhuohon@gmail.com>
2019-01-08 14:20:52 +0100

    fix db error

--- js/initReviewsDb.js
+++ js/initReviewsDb.js
@@ -1,5 +1,14 @@
 const reviewsDbPromise = idb.open('restaurant_reviews', 2, db => {
- 
+
+    if (!db.objectStoreNames.contains('reviews')) {
+      const reviewdb = db.createObjectStore('reviews', {
+        keyPath: 'unique',
+        autoIncrement: true 
+      });
+      reviewdb.createIndex("restaurant_id", "restaurant_id");
+    }
+  
+  
     if (!db.objectStoreNames.contains("deferred-reviews")) {
       db.createObjectStore("deferred-reviews", {
         keyPath: "id"
